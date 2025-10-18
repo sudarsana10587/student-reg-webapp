6f5dedebf684d636c7ac239089b7a8afa9ec6c66
sudarsana10587 <sudarsana10587@gmail.com>
2025-10-18 22:44:12 +0530

    updated

--- src/main/java/com/rushi/controller/HomeController.java
+++ src/main/java/com/rushi/controller/HomeController.java
@@ -91,6 +91,6 @@ public class HomeController {
     }
 
     private List<String> getCourses() {
-        return Arrays.asList("DevOps", "AWS", "Azure");
+        return Arrays.asList("DevOps", "AWS", "Azure","Scripting");
     }
 }
